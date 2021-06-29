889c8ce28130318aab8e1cb559818d00ec68d6c2
morhi-2214 <kamejump2214@gmail.com>
2021-06-30 00:38:03 +0900

    別のメッセージを追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>{{msg2}}</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,8 +34,13 @@
 <script>
 export default {
   name: 'HelloWorld!!Feature!!!',
+  data() {
+    return {
+      msg2: "Googbye"
+    }
+  },
   props: {
-    msg: String
+    msg: String,
   }
 }
 </script>
